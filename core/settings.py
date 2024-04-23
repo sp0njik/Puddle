f520f90e88e31c14438975751fedbc4a82c319d5
Artak Sarkisyan <sarkisyan.artak92@gmail.com>
2024-04-23 12:51:10 +0300

    Create item app and templates

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'puddle',
+    'item',
 ]
 
 MIDDLEWARE = [
@@ -117,7 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
